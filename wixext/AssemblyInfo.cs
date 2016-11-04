7294d88af93f5ec20d3316aa6dc1a799fdf59096
nir.bar <nir.bar@panel-sw.com>
2016-11-04 12:29:16 +0200

    Change out folder

--- wixext/AssemblyInfo.cs
+++ wixext/AssemblyInfo.cs
@@ -8,3 +8,6 @@ using Microsoft.Tools.WindowsInstallerXml;
 
 [assembly: AssemblyTitle("PanelSw WiX Extension")]
 [assembly: AssemblyDefaultWixExtension(typeof(PanelSwWixExtension))]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+
