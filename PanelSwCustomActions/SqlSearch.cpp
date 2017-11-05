78fa38e5fc2efceedb13bd7bf5902d5b96aa3142
nir.bar <nir.bar@panel-sw.com>
2017-11-05 13:26:36 +0200

    SqlSearch: Ensure one row is selected at most

--- PanelSwCustomActions/SqlSearch.cpp
+++ PanelSwCustomActions/SqlSearch.cpp
@@ -2,7 +2,6 @@
 #include "stdafx.h"
 #include "../CaCommon/WixString.h"
 #include <sqlutil.h>
-#include <memutil.h>
 #include <atlbase.h>
 
 #define SqlSearchQuery L"SELECT `Property_`, `Server`, `Instance`, `Database`, `Username`, `Password`, `Query` FROM `PSW_SqlSearch`"
@@ -23,6 +22,8 @@ extern "C" __declspec( dllexport ) UINT SqlSearch(MSIHANDLE hInstall)
 	PMSIHANDLE hRecord;
 	bool bIgnoreErrors = false;
 	BYTE *pRowData = NULL;
+	DBCOLUMNINFO *pColInfo = NULL;
+	HROW *phRow = NULL;
 
 	hr = WcaInitialize(hInstall, __FUNCTION__);
 	BreakExitOnFailure(hr, "Failed to initialize");
@@ -41,8 +42,18 @@ extern "C" __declspec( dllexport ) UINT SqlSearch(MSIHANDLE hInstall)
 		BreakExitOnFailure(hr, "Failed to fetch record.");
 		if (pRowData)
 		{
-			MemFree(pRowData);
+			::CoTaskMemFree(pRowData);
 			pRowData = NULL;
+		}
+		if (pColInfo)
+		{
+			::CoTaskMemFree(pColInfo);
+			pColInfo = NULL;
+		}
+		if (phRow)
+		{
+			::CoTaskMemFree(phRow);
+			phRow = NULL;
 		}
 
 		// Get fields
@@ -54,20 +65,17 @@ extern "C" __declspec( dllexport ) UINT SqlSearch(MSIHANDLE hInstall)
 		CWixString szPassword;
 		CWixString szQuery;
 		IDBCreateSession *pDbSession = NULL;
-		DBROWCOUNT nRows = 0;
 		DBCOUNTITEM unRows = 0;
-		HROW phRow[1];
 		CComBSTR szError;
 		CComPtr<IRowset> pRowset;
 		CComPtr<IAccessor> pAccessor;
 		CComPtr<IColumnsInfo> pColumnsInfo;
-		DBCOLUMNINFO pColInfo[1];
 		DBORDINAL nColCount = 0;
 		CComBSTR szColNames;
-		DBBINDING pDbBinding[1];
-		DBCOLUMNDATA sColData;
+		DBBINDING sDbBinding;
 		DBCOLUMNDATA *pColumn = NULL;
 		HACCESSOR hAccessor = NULL;
+		DBBINDSTATUS nBindStatus = DBBINDSTATUS_OK;
 
 		hr = WcaGetRecordString(hRecord, eSqlSearchQueryQuery::Property_, (LPWSTR*)szProperty);
 		BreakExitOnFailure(hr, "Failed to get Property_.");
@@ -90,16 +98,17 @@ extern "C" __declspec( dllexport ) UINT SqlSearch(MSIHANDLE hInstall)
 		BreakExitOnFailure(hr, "Failed connecting to database");
 		BreakExitOnNull(pDbSession, hr, E_FAIL, "Failed connecting to database (NULL)");
 
-		hr = SqlSessionExecuteQuery(pDbSession, (LPCWSTR)szQuery, &pRowset, &nRows, &szError);
+		hr = SqlSessionExecuteQuery(pDbSession, (LPCWSTR)szQuery, &pRowset, NULL, &szError);
 		BreakExitOnFailure1(hr, "Failed executing query. %ls", (LPCWSTR)(LPWSTR)szError);
 		BreakExitOnNull(pRowset, hr, E_FAIL, "Failed executing query (NULL)");
-		BreakExitOnNull1((nRows <= 1), hr, E_INVALIDARG, "Query returned %i rows. Can only handle scalar queries", nRows);
 
-		hr = pRowset->GetNextRows(DB_NULL_HCHAPTER, 0, 1, &unRows, (HROW**)&phRow);
+		// Requesting 2 rows to ensure one is returned at most.
+		hr = pRowset->GetNextRows(DB_NULL_HCHAPTER, 0, 2, &unRows, &phRow);
 		BreakExitOnFailure(hr, "Failed to get result row");
+		BreakExitOnNull((unRows <= 1), hr, E_FAIL, "Query returned more than one row");
 
 		// Empty result ==> clear property
-		if ((hr == DB_S_ENDOFROWSET) || (unRows == 0))
+		if (unRows == 0)
 		{
 			WcaLog(LOGLEVEL::LOGMSG_STANDARD, "Query returned no results. Clearing property");
 
@@ -112,7 +121,7 @@ extern "C" __declspec( dllexport ) UINT SqlSearch(MSIHANDLE hInstall)
 		hr = pRowset->QueryInterface<IColumnsInfo>(&pColumnsInfo);
 		BreakExitOnFailure(hr, "Failed to get result column info");
 
-		hr = pColumnsInfo->GetColumnInfo(&nColCount, (DBCOLUMNINFO**)&pColInfo, &szColNames);
+		hr = pColumnsInfo->GetColumnInfo(&nColCount, &pColInfo, &szColNames);
 		BreakExitOnFailure(hr, "Failed to get result column info");
 		BreakExitOnNull1((nColCount <= 1), hr, E_INVALIDARG, "Query returned %i columns. Can only handle scalar queries", nColCount);
 
@@ -124,37 +133,41 @@ extern "C" __declspec( dllexport ) UINT SqlSearch(MSIHANDLE hInstall)
 			BreakExitOnFailure1(hr, "Failed clearing property '%ls'", (LPCWSTR)szProperty);
 			continue;
 		}
-		
-		pDbBinding[0].dwPart = DBPART_VALUE | DBPART_LENGTH | DBPART_STATUS;
-		pDbBinding[0].eParamIO = DBPARAMIO_NOTPARAM;
-		pDbBinding[0].iOrdinal = pColInfo[0].iOrdinal;
-		pDbBinding[0].wType = DBTYPE_WSTR;
-		pDbBinding[0].pTypeInfo = NULL;
-		pDbBinding[0].obValue = offsetof(DBCOLUMNDATA, bData);
-		pDbBinding[0].obLength = offsetof(DBCOLUMNDATA, dwLength);
-		pDbBinding[0].obStatus = offsetof(DBCOLUMNDATA, dwStatus);
-		pDbBinding[0].cbMaxLen = pColInfo[0].ulColumnSize;
-		pDbBinding[0].pObject = NULL;
-		pDbBinding[0].pBindExt = NULL;
-		pDbBinding[0].dwFlags = 0;
-		pDbBinding[0].dwMemOwner = DBMEMOWNER_CLIENTOWNED;
-		pDbBinding[0].bPrecision = 0;
-		pDbBinding[0].bScale = 0;
+
+		sDbBinding.dwPart = DBPART_VALUE | DBPART_LENGTH | DBPART_STATUS;
+		sDbBinding.eParamIO = DBPARAMIO_NOTPARAM;
+		sDbBinding.iOrdinal = pColInfo[0].iOrdinal;
+		sDbBinding.wType = DBTYPE_WSTR;
+		sDbBinding.pTypeInfo = NULL;
+		sDbBinding.obValue = offsetof(DBCOLUMNDATA, bData);
+		sDbBinding.obLength = offsetof(DBCOLUMNDATA, dwLength);
+		sDbBinding.obStatus = offsetof(DBCOLUMNDATA, dwStatus);
+		sDbBinding.cbMaxLen = pColInfo[0].ulColumnSize;
+		sDbBinding.pObject = NULL;
+		sDbBinding.pBindExt = NULL;
+		sDbBinding.dwFlags = 0;
+		sDbBinding.dwMemOwner = DBMEMOWNER_CLIENTOWNED;
+		sDbBinding.bPrecision = 0;
+		sDbBinding.bScale = 0;
 
 		// Create accessor
 		hr = pRowset->QueryInterface<IAccessor>(&pAccessor);
 		BreakExitOnFailure(hr, "Failed to get result row accessor");
 
-		hr = pAccessor->CreateAccessor(DBACCESSOR_ROWDATA, 1, pDbBinding, 0, &hAccessor, NULL);
-		BreakExitOnFailure(hr, "Failed to create column data accessor");
+		hr = pAccessor->CreateAccessor(DBACCESSOR_ROWDATA, 1, &sDbBinding, sizeof(DBCOLUMNDATA), &hAccessor, &nBindStatus);
+		BreakExitOnFailure1(hr, "Failed to create column data accessor (DBBINDSTATUS=%i)", nBindStatus);
 
-		pRowData = (BYTE*)MemAlloc(pDbBinding[0].cbMaxLen + sizeof(DBCOLUMNDATA), TRUE);
+		pRowData = (BYTE*)::CoTaskMemAlloc(sDbBinding.cbMaxLen + sizeof(DBCOLUMNDATA));
 		BreakExitOnNull(pRowData, hr, E_FAIL, "Failed to allocate memory");
 
 		hr = pRowset->GetData(phRow[0], hAccessor, pRowData);
 		BreakExitOnFailure(hr, "Failed to get row data");
 
-		pColumn = (DBCOLUMNDATA*)(pRowData + pDbBinding[0].obLength);
+		hr = pAccessor->ReleaseAccessor(hAccessor, NULL);
+		BreakExitOnFailure(hr, "Failed releasing accessor");
+		hAccessor = NULL;
+
+		pColumn = (DBCOLUMNDATA*)(pRowData + sDbBinding.obLength);
 		switch (pColumn->dwStatus)
 		{
 		case DBSTATUS_S_ISNULL:
@@ -189,7 +202,8 @@ extern "C" __declspec( dllexport ) UINT SqlSearch(MSIHANDLE hInstall)
 			hr = -::abs((long)pColumn->dwStatus);
 			BreakExitOnFailure(hr, "Failed getting column data");
 			break;
-		}	}
+		}
+	}
 
 	hr = ERROR_SUCCESS;
 	WcaLog(LOGMSG_STANDARD, "Done.");
@@ -197,8 +211,18 @@ extern "C" __declspec( dllexport ) UINT SqlSearch(MSIHANDLE hInstall)
 LExit:
 	if (pRowData)
 	{
-		MemFree(pRowData);
+		::CoTaskMemFree(pRowData);
 		pRowData = NULL;
+	}
+	if (pColInfo)
+	{
+		::CoTaskMemFree(pColInfo);
+		pColInfo = NULL;
+	}
+	if (phRow)
+	{
+		::CoTaskMemFree(phRow);
+		phRow = NULL;
 	}
 
 	er = SUCCEEDED(hr) ? ERROR_SUCCESS : ERROR_INSTALL_FAILURE;
