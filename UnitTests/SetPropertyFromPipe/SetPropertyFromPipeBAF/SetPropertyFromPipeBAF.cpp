9bfd969709b5fbd863dc95984337da77afcf23dc
nir.bar <nir.bar@panel-sw.com>
2018-06-12 10:43:49 +0300

    UT- create pipe and send property.

--- UnitTests/SetPropertyFromPipe/SetPropertyFromPipeBAF/SetPropertyFromPipeBAF.cpp
+++ UnitTests/SetPropertyFromPipe/SetPropertyFromPipeBAF/SetPropertyFromPipeBAF.cpp
@@ -3,9 +3,16 @@
 #include "IBootstrapperBAFunction.h"
 #include <rpc.h>
 #include <strutil.h>
+#include <string>
+#include "setPropertyFromPipeDetails.pb.h"
 #pragma comment (lib, "balutil.lib")
 #pragma comment (lib, "dutil.lib")
 #pragma comment (lib, "Rpcrt4.lib")
+using namespace ::com::panelsw::ca;
+
+#define PROP_NAME	L"MY_PROP"
+#define PROP_VAL	L"MY_VALUE"
+
 
 class SetPropertyFromPipeBAF : public IBootstrapperBAFunction
 {
@@ -13,7 +20,33 @@ public:
 
 	HRESULT SendProperty()
 	{
-		return S_OK;
+		HRESULT hr = S_OK;
+		BOOL bRes = TRUE;
+		SetPropertyFromPipeDetails details;
+		property *prop;
+		std::string buffer;
+		DWORD dwSize = 0;
+
+		prop = details.add_properties();
+		BalExitOnNullWithLastError(prop, hr, "Failed allocating property for pipe message");
+
+		prop->set_name(PROP_NAME, ARRAYSIZE(PROP_NAME));
+		prop->set_value(PROP_VAL, ARRAYSIZE(PROP_VAL));
+		BalLog(BOOTSTRAPPER_LOG_LEVEL::BOOTSTRAPPER_LOG_LEVEL_STANDARD, "Sending name %ls, value %ls", (LPCWSTR)prop->name().data(), (LPCWSTR)prop->value().data());
+
+		bRes = details.SerializeToString(&buffer);
+		BalExitOnNullWithLastError(bRes, hr, "Failed serializing message");
+
+		bRes = ::ConnectNamedPipe(hPipe_, nullptr);
+		BalExitOnNullWithLastError(bRes, hr, "Failed connecting client to pipe");
+
+		bRes = ::WriteFile(hPipe_, buffer.data(), buffer.size(), &dwSize, nullptr);
+		BalExitOnNullWithLastError(bRes, hr, "Failed writing to pipe");
+		BalExitOnNullWithLastError((dwSize == buffer.size()), hr, "Failed writing to pipe (incompatible size)");
+
+	LExit:
+
+		return hr;
 	}
 
 	STDMETHODIMP OnDetect() 
@@ -30,11 +63,14 @@ public:
 			BalExitOnFailure(hr, "Failed creating pipe ID");
 		}
 
-		hr = ::UuidToStringW(&pipeId, &szPipeUuid);
+		hr = ::UuidToString(&pipeId, &szPipeUuid);
 		BalExitOnFailure(hr, "Failed converting pipe ID to string");
 
 		hr = StrAllocFormatted(&szLocalPipeName, L"\\\\.\\pipe\\%s", szPipeUuid);
 		BalExitOnFailure(hr, "Failed formatting string");
+
+		hPipe_ = ::CreateNamedPipe(szLocalPipeName, PIPE_ACCESS_OUTBOUND | FILE_FLAG_FIRST_PIPE_INSTANCE, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT | PIPE_REJECT_REMOTE_CLIENTS, 1, 0, 0, 0, nullptr);
+		BalExitOnNullWithLastError1((hPipe_ != INVALID_HANDLE_VALUE), hr, "Failed creating pipe '%ls'", szLocalPipeName);
 
 		hr = pEngine_->SetVariableString(L"PIPE_NAME", szLocalPipeName);
 		BalExitOnFailure(hr, "Failed setting variable");
@@ -46,7 +82,7 @@ public:
 		ReleaseStr(szLocalPipeName);
 		if (szPipeUuid)
 		{
-			::RpcStringFreeW(&szPipeUuid);
+			::RpcStringFree(&szPipeUuid);
 		}
 
 		return hr;
