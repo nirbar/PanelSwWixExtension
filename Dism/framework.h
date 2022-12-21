8e783457570adb0cec012c3ea43bf72c8e327a4c
Nir Bar <nir.bar@cellebrite.com>
2022-12-21 11:04:26 +0200

    Migrate Dism project ti WiX v4

--- Dism/framework.h
+++ Dism/framework.h
@@ -1,15 +1,13 @@
 #pragma once
 
-#include "targetver.h"
-
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
+// Windows Header Files
 #include <windows.h>
 #include <strsafe.h>
 #include <msiquery.h>
 
 // WiX Header Files:
 #include <wcautil.h>
-
-
-// TODO: reference additional headers your program requires here
+#include <fileutil.h>
+#include <strutil.h>
+#include <memutil.h>
