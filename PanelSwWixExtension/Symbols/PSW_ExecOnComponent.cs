ba6b422729d887f209089fec1d74296d0171bf71
Nir Bar <nir.bar@panel-sw.co.il>
2023-07-10 11:17:35 +0300

    Add ErrorHandling="promptAlways" option, to prompt user to continue/cancel even on command success. Currently understood by "ExecOn" only

--- PanelSwWixExtension/Symbols/PSW_ExecOnComponent.cs
+++ PanelSwWixExtension/Symbols/PSW_ExecOnComponent.cs
@@ -25,7 +25,7 @@ namespace PanelSw.Wix.Extensions.Symbols
                     new ColumnDefinition(nameof(Command), ColumnType.Localized, 0, false, false, ColumnCategory.Formatted, modularizeType: ColumnModularizeType.Property),
                     new ColumnDefinition(nameof(WorkingDirectory), ColumnType.Localized, 0, false, true, ColumnCategory.Formatted, modularizeType: ColumnModularizeType.Property),
                     new ColumnDefinition(nameof(Flags), ColumnType.Number, 4, false, false, ColumnCategory.Integer, minValue: 0, maxValue: ushort.MaxValue),
-                    new ColumnDefinition(nameof(ErrorHandling), ColumnType.Number, 2, false, false, ColumnCategory.Integer, minValue: 0, maxValue: 2),
+                    new ColumnDefinition(nameof(ErrorHandling), ColumnType.Number, 2, false, false, ColumnCategory.Integer, minValue: 0, maxValue: 3),
                     new ColumnDefinition(nameof(Order), ColumnType.Number, 4, false, false, ColumnCategory.Integer, minValue: 0, maxValue: int.MaxValue),
                     new ColumnDefinition(nameof(User_), ColumnType.String, 72, false, true, ColumnCategory.Identifier, modularizeType: ColumnModularizeType.Column, keyTable: "Wix4User", keyColumn: 1),
                 };
