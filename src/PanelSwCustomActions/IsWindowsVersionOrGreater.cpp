5519bdd33c55423809ea316bf00ee76fa366407e
Nir Bar <nir.bar@panel-sw.co.il>
2024-11-06 11:02:03 +0200

    Fix IsWindowsVersionOrGreater: Use Kernel32.dll product version rather than file version. It correctly ientifies Windows OS version number

--- src/PanelSwCustomActions/IsWindowsVersionOrGreater.cpp
+++ src/PanelSwCustomActions/IsWindowsVersionOrGreater.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 
+static HRESULT DAPI GetKernel32ProductVersion(ULARGE_INTEGER* pullKernelProductVersion);
+
 extern "C" UINT __stdcall IsWindowsVersionOrGreater(MSIHANDLE hInstall)
 {
 	HRESULT hr = S_OK;
@@ -7,9 +9,8 @@ extern "C" UINT __stdcall IsWindowsVersionOrGreater(MSIHANDLE hInstall)
 	BOOL bRes = TRUE;
 	PMSIHANDLE hView;
 	PMSIHANDLE hRecord;
-	HMODULE hKernel32 = NULL;
-	CWixString szKernel32Path, szKernel32Version;
-	ULARGE_INTEGER ullKernelVersion;
+	CWixString szKernel32Version;
+	ULARGE_INTEGER ullKernelVersion = {};
 
 	hr = WcaInitialize(hInstall, __FUNCTION__);
 	ExitOnFailure(hr, "Failed to initialize");
@@ -19,21 +20,15 @@ extern "C" UINT __stdcall IsWindowsVersionOrGreater(MSIHANDLE hInstall)
 	ExitOnFailure(hr, "Failed to check if table exists 'PSW_IsWindowsVersionOrGreater'");
 	ExitOnNull((hr == S_OK), hr, E_FAIL, "Table does not exist 'PSW_IsWindowsVersionOrGreater'. Have you authored 'PanelSw:IsWindowsVersionOrGreater' entries in WiX code?");
 	
-	hKernel32 = ::GetModuleHandleW(L"kernel32");
-	ExitOnNullWithLastError(hKernel32, hr, "Failed to get module handle for kernel32.");
-
-	hr = PathForCurrentProcess((LPWSTR*)szKernel32Path, hKernel32);
-	ExitOnFailure(hr, "Failed to get full path of Kernel32.dll");
-
-	hr = FileVersion((LPCWSTR)szKernel32Path, &ullKernelVersion.HighPart, &ullKernelVersion.LowPart);
-	ExitOnFailure(hr, "Failed to get version of Kernel32.dll");
+	hr = GetKernel32ProductVersion(&ullKernelVersion);
+	ExitOnFailure(hr, "Failed to get product version of Kernel32.dll");
 
 	hr = FileVersionToStringEx(ullKernelVersion.QuadPart, (LPWSTR*)szKernel32Version);
-	ExitOnFailure(hr, "Failed to parse version of Kernel32.dll");
-	WcaLog(LOGMSG_STANDARD, "Detected version '%ls' of '%ls'", (LPCWSTR)szKernel32Version, (LPCWSTR)szKernel32Path);
+	ExitOnFailure(hr, "Failed to parse product version of Kernel32.dll");
+	WcaLog(LOGMSG_STANDARD, "Detected product version '%ls' of Kernel32.dll", (LPCWSTR)szKernel32Version);
 
 	// Execute view
-	hr = WcaOpenExecuteView(L"SELECT `Property_`, `Version` FROM `PSW_IsWindowsVersionOrGreater`", &hView);
+	hr = WcaOpenExecuteView(L"SELECT `Property_`, `MinVersion`, `MaxVersion` FROM `PSW_IsWindowsVersionOrGreater`", &hView);
 	ExitOnFailure(hr, "Failed to execute MSI SQL query");
 
 	// Loop
@@ -41,18 +36,26 @@ extern "C" UINT __stdcall IsWindowsVersionOrGreater(MSIHANDLE hInstall)
 	{
 		ExitOnFailure(hr, "Failed to fetch record.");
 		
-		CWixString szProperty, szMinVersion;
-		ULARGE_INTEGER ullMinVersion;
+		CWixString szProperty, szMinVersion, szMaxVersion;
+		ULARGE_INTEGER ullMinVersion = {}, ullMaxVersion = {};
 
 		hr = WcaGetRecordString(hRecord, 1, (LPWSTR*)szProperty);
 		ExitOnFailure(hr, "Failed to get Property_.");
 		hr = WcaGetRecordString(hRecord, 2, (LPWSTR*)szMinVersion);
 		ExitOnFailure(hr, "Failed to get Version.");
+		hr = WcaGetRecordString(hRecord, 3, (LPWSTR*)szMaxVersion);
+		ExitOnFailure(hr, "Failed to get Version.");
 
 		hr = FileVersionFromString((LPCWSTR)szMinVersion, &ullMinVersion.HighPart, &ullMinVersion.LowPart);
 		ExitOnFailure(hr, "Failed to parse minimal version '%ls' for '%ls'.", (LPCWSTR)szMinVersion, (LPCWSTR)szProperty);
 
-		if (ullKernelVersion.QuadPart >= ullMinVersion.QuadPart)
+		if (!szMaxVersion.IsNullOrEmpty())
+		{
+			hr = FileVersionFromString((LPCWSTR)szMaxVersion, &ullMaxVersion.HighPart, &ullMaxVersion.LowPart);
+			ExitOnFailure(hr, "Failed to parse maximal version '%ls' for '%ls'.", (LPCWSTR)szMinVersion, (LPCWSTR)szProperty);
+		}
+
+		if ((ullKernelVersion.QuadPart >= ullMinVersion.QuadPart) && (szMaxVersion.IsNullOrEmpty() || (ullKernelVersion.QuadPart <= ullMaxVersion.QuadPart)))
 		{
 			hr = WcaSetIntProperty((LPCWSTR)szProperty, 1);
 			ExitOnFailure(hr, "Failed to set property.");
@@ -63,4 +66,34 @@ extern "C" UINT __stdcall IsWindowsVersionOrGreater(MSIHANDLE hInstall)
 LExit:
 	er = SUCCEEDED(hr) ? ERROR_SUCCESS : ERROR_INSTALL_FAILURE;
 	return WcaFinalize(er);
+}
+
+static HRESULT DAPI GetKernel32ProductVersion(ULARGE_INTEGER* pullKernelProductVersion)
+{
+	HRESULT hr = S_OK;
+	UINT cbVerBuffer = 0;
+	LPVOID pVerBuffer = nullptr;
+	VS_FIXEDFILEINFO* pvsFileInfo = nullptr;
+	UINT cbFileInfo = 0;
+	BOOL bRes = TRUE;
+
+	cbVerBuffer = ::GetFileVersionInfoSize(L"Kernel32", nullptr);
+	ExitOnNullWithLastError(cbVerBuffer, hr, "Failed to get Kernel32.dll version info size");
+
+	pVerBuffer = ::MemAlloc(cbVerBuffer, TRUE);
+	ExitOnNullWithLastError(pVerBuffer, hr, "Failed to allocate memory");
+
+	bRes = ::GetFileVersionInfo(L"Kernel32", 0, cbVerBuffer, pVerBuffer);
+	ExitOnNullWithLastError(bRes, hr, "Failed to get Kernel32.dll version info");
+
+	bRes = ::VerQueryValue(pVerBuffer, L"\\", (void**)&pvsFileInfo, &cbFileInfo);
+	ExitOnNullWithLastError(bRes, hr, "Failed to get Kernel32.dll version");
+
+	pullKernelProductVersion->HighPart = pvsFileInfo->dwProductVersionMS;
+	pullKernelProductVersion->LowPart = pvsFileInfo->dwProductVersionLS;
+
+LExit:
+	ReleaseMem(pVerBuffer);
+
+	return hr;
 }
