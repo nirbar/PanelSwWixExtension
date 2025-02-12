6781b049a02ae8ccc68cea5a10efbff486ccb0ce
Nir Bar <nir.bar@panel-sw.co.il>
2025-02-12 11:08:02 +0200

    ZipFile glob pattern- support glob standard as in https://code.visualstudio.com/docs/editor/glob-patterns

--- src/PanelSwCustomActions/FileGlobFilter.h
+++ src/PanelSwCustomActions/FileGlobFilter.h
@@ -6,6 +6,15 @@ class CFileGlobFilter
 	: public IFileFilter
 {
 public:
+	/* Supported glob format as in https://code.visualstudio.com/docs/editor/glob-patterns
+		- / to separate path segments
+		- * to match zero or more characters in a path segment
+		- ? to match on one character in a path segment
+		- ** to match any number of path segments, including none
+		- {} to group conditions (for example {**\*.html, **\*.txt} matches all HTML and text files)
+		- [] to declare a range of characters to match (example.[0-9] to match on example.0, example.1, …)
+		- [!...] to negate a range of characters to match (example.[!0-9] to match on example.a, example.b, but not example.0)
+	*/
 	HRESULT Initialize(LPCWSTR szBaseFolder, LPCWSTR szFilter, bool bRecursive) override;
 	
 	HRESULT IsMatch(LPCWSTR szFilePath) const override;
